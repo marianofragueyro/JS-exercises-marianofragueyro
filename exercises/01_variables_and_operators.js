fa3c102de841a13c44c427ce776a6b706d31a97d
Mariano Fragueyro <marianofragueyro@gmail.com>
2021-10-08 14:28:48 -0300

    BaSD: js exercises 01 done

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,20 @@
+// 1.A. 
+var number1 = 23
+var number2 = 17
+var result = number1 + number2
+console.log('The sum of the first and second variables are ' + result)
+
+//1.B.
+var firstName = 'Mariano '
+var lastName = 'Fragueyro'
+var fullname = firstName + lastName
+
+console.log('My name is ' + fullname)
+
+//1.C.
+var quote = 'This is the'
+var secondQuote = 'third excercise'
+var quoteLength = quote.length
+var secondQuoteLenght = secondQuote.length
+var totalLength = quoteLength + secondQuoteLenght
+console.log( 'The total length of both strings is ' + totalLength)
